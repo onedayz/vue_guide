d207c327a965e42a2cd9a35f2995f4883156b58c
Grey <blind1442@gmail.com>
2021-04-06 04:29:18 +0900

    vue guide level 1 freeze

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    publicPath: '/',
+    publicPath: '',
     outputDir: 'docs',
     assetsDir: 'assets', // prefix 'dist/'
     filenameHashing: true,
